4c02fa4f37f29073ca898a49515ea54417591dca
v_shenningting <v_shenningting@baidu.com>
2018-12-16 15:21:20 +0800

    compoesr

---
+++ dump.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v_shenningting
+ * Date: 2018/12/16
+ * Time: 14:42
+ */
+
+if(!function_exists('d')){
+    function d($data){
+        echo "<pre>";
+        print_r($data);
+    }
+}
